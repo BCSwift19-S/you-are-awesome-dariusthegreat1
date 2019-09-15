1ddba49db8f08bc3d312393bf6de42d50de05215
Nick Haidari <haidari@bc.edu>
2019-09-15 10:56:11 -0400

    identical to prof, errors develop

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -7,16 +7,20 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var awesomeImageView: UIImageView!
     @IBOutlet weak var messageLabel: UILabel!
+    var awesomePlayer = AVAudioPlayer()
+    var soundName = "sound0"
     var index = 0
-    
+    var imageIndex = -1
+    let numberOfImages = 10
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     @IBAction func showMessagePressed(_ sender: UIButton) {
     
@@ -28,6 +32,7 @@ class ViewController: UIViewController {
                     "You are da bomb!",
                     "Hey, Fabulous!",
                     "You are tremendous"]
+        
     //var newIndex = -1
         var newIndex: Int // declres but doesnt initialize
         repeat{
@@ -36,7 +41,32 @@ class ViewController: UIViewController {
     
     index = newIndex
     messageLabel.text = messages[index]
+    
+    repeat {
+        newIndex = Int.random(in: 0..<numberOfImages)
+    } while imageIndex == newIndex
         
+    imageIndex = newIndex
+    awesomeImageView.image = UIImage(named: "image\(imageIndex)" )
+    }
+    
+    //sounds
+    
+    
+    
+    //try to load in sound name file
+    if let sound = NSDataAsset(name: soundName){
+        //checking if sound.data is a soundfile
+        do {
+            try awesomePlayer = AVAudioPlayer(data: sound.data)
+            awesomePlayer.play()
+        }catch{
+            //if sound.data is not valid audio file,
+            print("Error: data in \(soundname) couldn't be played as sound.")
+        }
+    }else{
+        //if something didn't work, tell why
+    print("ERROR: file \(soundName) didn't load")
     }
     
 //    messageLabel.text = messages[index]
