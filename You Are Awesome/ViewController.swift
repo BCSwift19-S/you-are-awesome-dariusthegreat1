081fa40548677652c9946fcf395000c41d39a482
Nick Haidari <haidari@bc.edu>
2019-09-15 13:03:25 -0400

    2.17 sounds and photos

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -13,69 +13,79 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var awesomeImageView: UIImageView!
     @IBOutlet weak var messageLabel: UILabel!
+    
     var awesomePlayer = AVAudioPlayer()
-    var soundName = "sound0"
+    var soundIndex = -1
     var index = 0
     var imageIndex = -1
     let numberOfImages = 10
+    let numberOfSounds = 6
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    @IBAction func showMessagePressed(_ sender: UIButton) {
     
-    let messages = ["You are awesome",
-                    "You are Great",
-                    "You are Fantastic",
-                    "When the Genius Bar needs help they call you",
-                    "You brighten my day",
-                    "You are da bomb!",
-                    "Hey, Fabulous!",
-                    "You are tremendous"]
+    @IBAction func showMessagePressed(_ sender: UIButton) {
         
-    //var newIndex = -1
+        let messages = ["You are awesome",
+                        "You are Great",
+                        "You are Fantastic",
+                        "When the Genius Bar needs help they call you",
+                        "You brighten my day",
+                        "You are da bomb!",
+                        "Hey, Fabulous!",
+                        "You are tremendous"]
+        
+        //var newIndex = -1
         var newIndex: Int // declres but doesnt initialize
         repeat{
             newIndex = Int.random(in: 0..<messages.count)
-        }while index == newIndex
-    
-    index = newIndex
-    messageLabel.text = messages[index]
-    
-    repeat {
-        newIndex = Int.random(in: 0..<numberOfImages)
-    } while imageIndex == newIndex
+        } while index == newIndex
         
-    imageIndex = newIndex
-    awesomeImageView.image = UIImage(named: "image\(imageIndex)" )
-    }
-    
-    //sounds
-    
-    
-    
-    //try to load in sound name file
-    if let sound = NSDataAsset(name: soundName){
-        //checking if sound.data is a soundfile
-        do {
-            try awesomePlayer = AVAudioPlayer(data: sound.data)
-            awesomePlayer.play()
-        }catch{
-            //if sound.data is not valid audio file,
-            print("Error: data in \(soundname) couldn't be played as sound.")
+        index = newIndex
+        messageLabel.text = messages[index]
+        
+        repeat {
+            newIndex = Int.random(in: 0..<numberOfImages)
+        } while imageIndex == newIndex
+        
+        imageIndex = newIndex
+        awesomeImageView.image = UIImage(named: "image\(imageIndex)" )
+        
+        //sounds
+        
+        var soundName = "sound\(soundIndex)"
+        
+        repeat {
+            newIndex = Int.random(in: 0..<numberOfSounds)
+        } while soundIndex == newIndex
+        
+        soundIndex = newIndex
+        
+        //try to load in sound name file
+        
+        if let sound = NSDataAsset(name: soundName) {
+            
+            //checking if sound.data is a soundfile
+            do {
+                try awesomePlayer = AVAudioPlayer(data: sound.data)
+                awesomePlayer.play()
+            }catch{
+                //if sound.data is not valid audio file,
+                print("Error: data in \(soundName) couldn't be played as sound.")
+            }
+        } else {
+            //if something didn't work, tell why
+            print("ERROR: file \(soundName) didn't load")
         }
-    }else{
-        //if something didn't work, tell why
-    print("ERROR: file \(soundName) didn't load")
+        
+        //    messageLabel.text = messages[index]
+        //
+        //    if index == messages.count - 1{
+        //        index = 0
+        //    }else{
+        //        index = index +1
+        //    }
     }
-    
-//    messageLabel.text = messages[index]
-//
-//    if index == messages.count - 1{
-//        index = 0
-//    }else{
-//        index = index +1
-//    }
-
 }
 
